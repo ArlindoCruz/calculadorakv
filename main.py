10ce9463ed170bbbaa313f65a58c04b6b2d5daa5
ArlindoCruz <162373742+ArlindoCruz@users.noreply.github.com>
2024-05-14 17:38:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from kivy import *
 from kivy.app import App
-class Estudo1App(App):
-    pass
-janela = Estudo1App()
-janela.run()
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+class app (App):
+    def build(self):
+
+        coiso = BoxLayout(orientation = ('vertical'))
+    
+        texto1 = Label(text = 'olá senai', font_size = ('50pt'))
+    
+        texto2 = Label(text = 'olá sesi', font_size = ('50'), color= (1,0,0,1))
+    
+        coiso.add_widget(texto1)
+        coiso.add_widget(texto2)
+        return coiso
+    
+if __name__ == '__main__':
+    app().run()
+   
+
